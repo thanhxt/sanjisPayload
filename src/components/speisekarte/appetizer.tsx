c9954a7f6c5b6fab8a4cb1645f1ad3bc15252d17
thanhxt <thanhtran_xuan@hotmail.de>
2025-06-17 14:01:25 +0200

    adjust menu design of maindish and appetizer

--- src/components/speisekarte/appetizer.tsx
+++ src/components/speisekarte/appetizer.tsx
@@ -1,88 +1,138 @@
-import Image from "next/image";
+"use client";
+import HeroImage from "../hero";
 import HeroText from "../hero-text";
+import Image from "next/image";
+import { useLanguage } from "../contexts/language-context";
+
 
 export default function Appetizer() {
+    const menuItems = [
+        {
+            title: "spicy salmon tatare",
+            price: "19.9",
+            descriptionDE: "Lachswürfel | Chilli-Mayo | Sesam | Koriander | Zwiebeln | Reis-Nori Cracker",
+            descriptionEN: "salmon cubes | chilli mayo | sesame | coriander | onions | rice-nori cracker",
+        },
+        {
+            title: "flamed yellowfin tuna tataki",
+            price: "19.9",
+            descriptionDE: "flambierter Gelbschwanzthunfisch | Kimchi | Wasabi Mayo | Teriyaki | Sesam",
+            descriptionEN: "flambéed yellowfin tuna | kimchi | wasabi mayo | teriyaki | sesame",
+        },
+        {
+            title: "truffled wagyu carpaccio",
+            price: "24.9",
+            descriptionDE: "hauchdünne Wagyu scheiben | Trüffel-Mayo | Rucola | Parmesan | Saisontrüffel",
+            descriptionEN: "thin wagyu slices | truffle-mayo | rocket | parmesan | seasonal truffle",
+        },
+        {
+            title: "crispy chilli prawns",
+            price: "19.9",
+            descriptionDE: "panierte Black Tiger Garnelen | Chilli-Mayo | Chilliflocken",
+            descriptionEN: "crispy black tiger prawns | chilli mayo | chilli flakes",
+        },
+        {
+            title: "wagyu beef la lot",
+            price: "14.9",
+            descriptionDE: "Wildbetelblätter | Wagyu hack | Erdnüsse | Röstzwiebeln | Fischsauce",
+            descriptionEN: "wild betel leaves | minced wagyu | peanuts | fried onions | fish sauce",
+        },
+        {
+            title: "green mango duck salad",
+            price: "19.9",
+            descriptionDE: "medium gegrillte Barbarieentenbrust | Koriander | Thai-Basilikum | Zwiebeln | Erdnüsse",
+            descriptionEN: "medium grilled barbary duck breast | coriander | thai basil | onions | peanuts",
+        },
+        {
+            title: "softshellcrabs",
+            price: "19.9",
+            descriptionDE: "Butterkrebse | Frühlingszwiebeln | Pfeffer",
+            descriptionEN: "softshellcrabs | spring onions | pepper",
+        },
+        {
+            title: "edamame",
+            price: "8.9",
+            descriptionDE: "japanische Sojabohnen | Sesam | Maldonsalz",
+            descriptionEN: "japanese soybeans | sesame | maldon salt",
+        },
+    ];
+    const starterSharing = [
+        { title: "spicy salmon tatare" },
+        { title: "flamed yellowfin tuna tataki" },
+        { title: "wagyu beef la lot" },
+        { title: "green mango duck salad" },
+    ];
+
+    const { language } = useLanguage();
+
     return (
-        <>
-        <div>
-            {/* Hero Section */}
-            <div className="relative w-full h-[60vh] flex items-center justify-center overflow-hidden mb-0 bg-black">
+        <div className="bg-black text-white">
+            {/* Hero Image */}
+            <div className="relative w-full h-[50vh] md:h-[60vh] flex items-center justify-center overflow-hidden mb-0 bg-black z-0">
                 <Image
-                    src="/Sanjis_Julio-27.jpg"
-                    alt="Background"
-                    width={1000}
-                    height={1000}
-                    className="absolute w-full h-full object-cover blur-md brightness-50 z-10"
+                src="/Sanjis_Julio-27.jpg" 
+                alt="Appetizer" 
+                width={1000} 
+                height={1000}
+                className="absolute w-full h-full object-cover blur-md brightness-50 z-0"
                 />
-                <HeroText title="Vorspeise" titleEn="Appetizer" />
+                <HeroText title="Vorspeisen" titleEn="Appetizers" />
             </div>
-        </div>
-        <div className="bg-zinc-900 text-white py-16 min-h-screen">
-            <div className="max-w-[1200px] text-lg mx-auto px-4">
-                <h1 className="text-center text-4xl mb-6 font-bold">APPETIZER</h1>
-                <div className="flex gap-12 flex-wrap justify-between">
-                    {/* Left Column */}
-                    <div className="flex-1 min-w-[350px]">
-                        <div className="mb-8">
-                            <span>Spicy Salmon</span> 10.9<br />
-                            Lachs würfel | Chilli-Mayo | Sesam | Koriander | Zwiebeln | Reis-Nori Cracker<br />
-                            <span className="text-gray-500 text-sm">salmon cubes | chilli mayo | sesame | coriander | onions | rice-nori crackers</span>
-                        </div>
-                        <div className="mb-8">
-                            <span>Flamed Yellowfin Tuna Tataki</span> 10.9<br />
-                            flambierter Gelbschwanzthunfisch | Kimchi | Wasabi Mayo | Teriyaki | Sesam<br />
-                            <span className="text-gray-500 text-sm">flambéed yellowfin tuna | kimchi | wasabi mayo | teriyaki | sesame</span>
-                        </div>
-                        <div className="mb-8">
-                            <span>Truffled Wagyu Carpaccio</span> 24.9<br />
-                            hauchdünne Wagyu scheiben | Trüffel-Mayo | Rucola | Parmesan | Saisontrüffel<br />
-                            <span className="text-gray-500 text-sm">thin wagyu slices | truffle-mayo | rocket | parmesan | seasonal truffle</span>
-                        </div>
-                        <div className="mb-8">
-                            <span>Crispy Chilli Prawns</span> 19.9<br />
-                            panierte Black Tiger Garnelen | Chilli-Mayo | Chilliflocken<br />
-                            <span className="text-gray-500 text-sm">crispy black tiger prawns | chilli mayo | chilli flakes</span>
-                        </div>
-                        <div className="mb-8">
-                            <span>Wagyu Beef La Lot</span> 14.9<br />
-                            Wildbetelblätter | Wagyu hack | Erdnüsse | Röstzwiebeln | Fischsauce<br />
-                            <span className="text-gray-500 text-sm">wild betel leaves | minced wagyu | peanuts | fried onions | fish sauce</span>
-                        </div>
-                    </div>
-                    {/* Right Column */}
-                    <div className="flex-1 min-w-[350px]">
-                        <div className="mb-8">
-                            <span>Green Mango Duck Salad</span> 10.9<br />
-                            medium gegrillte Barbarieentenbrust | Koriander | Thai-Basilikum | Zwiebeln | Erdnüsse<br />
-                            <span className="text-gray-500 text-sm">medium grilled barbary duck breast | coriander | thai basil | onions | peanuts</span>
-                        </div>
-                        <div className="mb-8">
-                            <span>Soft Shell Crabs</span> 10.9<br />
-                            Butterkrebs | Frühlingszwiebeln | Pfeffer<br />
-                            <span className="text-gray-500 text-sm">soft shell crabs | spring onions | pepper</span>
-                        </div>
-                        <div className="mb-8">
-                            <span>Edamame</span> 8.9<br />
-                            japanische Sojabohnen | Sesam | Maldonsalz<br />
-                            <span className="text-gray-500 text-sm">japanese soybeans | sesame | maldon salt</span>
-                        </div>
-                        <div className="mb-8">
-                            <span>Pléiade Poget No 3 Oyster</span> 3p / 18  6p / 36<br />
-                        </div>
-                        {/* Sauces Box */}
-                        <div className="bg-zinc-800 text-white border-2 border-gray-500 rounded-md mt-10 p-8 max-w-[350px]">
-                            <div className="font-bold text-xl mb-4">Classic Starter Sharing
-                                <span className="text-gray-500 text-sm block mt-1">startet ab 2 Personen – 25 P.P. • starts at 2 persons – 25 p.p.</span>
+
+            {/* Menu */}
+            <div className="flex flex-col md:flex-row bg-black text-white p-4 md:p-10 items-center justify-center">
+                <Image 
+                src="/vorspeiseSchrift.png" 
+                alt="Appetizer" 
+                width={170} 
+                height={120}
+                className="mb-6 md:mb-0 hidden md:block"
+                />
+
+                <div className="flex flex-col md:ml-10 w-full md:w-auto px-4 md:px-0">
+                    {menuItems.map((item, idx) => (
+                        <div key={idx} className="mb-6">
+                            <div className="flex flex-row items-center">
+                                <span className="text-xl md:text-2xl font-medium">{item.title}</span>
+                                <span className="text-base md:text-lg font-semibold ml-2">{item.price}</span>
                             </div>
-                            <div className="flex justify-between mb-2"><span>Wagyu Beef La Lot</span></div>
-                            <div className="flex justify-between mb-2"><span>French Mango Salat</span></div>
-                            <div className="flex justify-between mb-2"><span>Lachs Tatar</span></div>
-                            <div className="flex justify-between mb-2"><span>Yellowfin Tuna Tataki</span></div>
+                            <div className="flex flex-col">
+                                <p className="text-gray-400 mt-1 text-sm md:text-base">{language === "de" ? item.descriptionDE : item.descriptionEN}</p>
+                            </div>
                         </div>
+                    ))}
+                    <span className="text-xl md:text-2xl font-medium mb-6">Plediade Poget No 3 Oyster 3p./18 6p. 3</span>
+                </div>
+            </div>
+
+            {/* Starter Sharing Section */}
+            <div className="flex flex-col md:flex-row bg-black text-white p-4 md:p-10 items-center justify-center gap-6 md:gap-30">
+                <Image 
+                src="/Sanjis_Julio-27.jpg" 
+                alt="Appetizer" 
+                width={150} 
+                height={150}
+                className="rounded-full mb-6 md:mb-0"
+                />
+                <div className=" text-white border-2 border-black rounded-md mt-4 md:mt-10 p-6 md:p-8 w-full md:max-w-[350px]">
+                    <div className="font-bold text-2xl md:text-3xl mb-4">
+                        STARTER SHARING <br /> 
+                        <span className="text-base md:text-lg text-gray-500">
+                            {language === "de" ? "ab 2 Personen - 25 p.p." : "starts at 2 persons - 25 p.p."}
+                        </span>
+                    </div>
+                    {/* Starter Sharing */}
+                    <div className="flex flex-col">
+                    {starterSharing.map((item, idx) => (
+                        <div key={idx} className="mb-2">
+                            <div className="flex flex-row">
+                                <span className="text-base md:text-lg font-semibold">{item.title}</span>
+                            </div>
+                        </div>
+                    ))}
                     </div>
                 </div>
             </div>
         </div>
-        </>
-    )
+    );
 }
--- src/components/speisekarte/maindish.tsx
+++ src/components/speisekarte/maindish.tsx
@@ -1,75 +1,94 @@
+"use client"
 import Image from "next/image";
 import HeroText from "../hero-text";
+import { useLanguage } from "../contexts/language-context";
 
 export default function MainDish() {
+    const menuItems = [
+        {
+            title: "surf & turf",
+            price: "39.9",
+            descriptionDE: "100g Black Angus Roastbeef | Black Tiger Prawns | Saisongemüse | Jasminreis",
+            descriptionEN: "100g black angus striploin | black tiger prawns | seasonal vegetables | jasmine rice"
+        },
+        {
+            title: "lamb chops",
+            price: "44.9",
+            descriptionDE: "Lammkottlets (Neuseeland) | Wilder Brokkoli | getrüffeltes Kartoffelpüree",
+            descriptionEN: "lamb chops (New Zealand) | wild broccoli | truffled mashed potatoes"
+        },
+        {
+            title: "french duck red sin",
+            price: "34.9",
+            descriptionDE: "medium gegrillte Barbarieentenbrust | Saisongemüse | Rotwein-Hoisin Soße | Jasminreis",
+            descriptionEN: "medium grilled barbary duck breast | seasonal vegetables | red wine hoisin sauce | jasmine rice"
+        },
+        {
+            title: "salmon teriyaki",
+            price: "31.9",
+            descriptionDE: "Lachs | Saisongemüse | Teriyakisoße | Jasminreis",
+            descriptionEN: "salmon | seasonal vegetables | teriyaki sauce | jasmine rice"
+        },
+        {
+            title: "crispy angus tamarind",
+            price: "38.9",
+            descriptionDE: "panierte Black Angus Filetstreifen | grüner Spargel | Zuckerschoten | Zwiebeln | Tamarindensoße | Jasminreis",
+            descriptionEN: "crispy black angus filet strips | green asparagus | sugar snap peas | onions | tamarind sauce | jasmine rice"
+        },
+        {
+            title: "tiger prawns thai basil",
+            price: "39.9",
+            descriptionDE: "gegrillte Tiger Prawns | Saisongemüse | Thai-Basilikum | Jasminreis",
+            descriptionEN: "grilled tiger prawns | seasonal vegetables | thai-basil | jasmine rice"
+        },
+        {
+            title: "crispy tofu curry",
+            price: "23.9",
+            descriptionDE: "Tofu | Saisongemüse | rote Kokos-Curry Soße | Jasminreis",
+            descriptionEN: "tofu | seasonal vegetables | red coconut-curry sauce | jasmine rice"
+        }
+    ]
+    const { language } = useLanguage();
     return (
-        <>
-        <div>
-            {/* Hero Section */}
-            <div className="relative w-full h-[60vh] flex items-center justify-center overflow-hidden mb-0 bg-black">
-                <Image
-                    src="/Sanjis_Julio-14.jpg"
-                    alt="Background"
-                    width={1000}
-                    height={1000}
-                    className="absolute w-full h-full object-cover blur-md brightness-50 z-10"
-                />
-                <HeroText title="Hauptspeise" titleEn="Main Dish" />
+        <div className="bg-black text-white">
+            <div>
+                {/* Hero Section */}
+                <div className="relative w-full h-[60vh] flex items-center justify-center overflow-hidden mb-0 bg-black">
+                    <Image
+                        src="/Sanjis_Julio-14.jpg"
+                        alt="Background"
+                        width={1000}
+                        height={1000}
+                        className="absolute w-full h-full object-cover blur-md brightness-30 z-10"
+                    />
+                    <HeroText title="Hauptspeise" titleEn="Main Dish" />
+                </div>
             </div>
-        </div>
-        <div className="bg-zinc-900 text-white py-16 min-h-screen">
-            <div className="max-w-[1200px] text-lg mx-auto px-4">
-                <h1 className="text-center text-4xl mb-6 font-bold">MAIN DISH</h1>
-                <div className="flex gap-12 flex-wrap justify-between">
-                    {/* Left Column */}
-                    <div className="flex-1 min-w-[350px]">
-                        <div className="mb-8">
-                            <span>surf &amp; turf</span> 39.9<br />
-                            100g Black Angus Roastbeef |
-                            Black Tiger Prawns | Saisongemüse | Jasminreis<br />
-                            <span className="text-gray-500 text-sm">100g black angus striploin | black tiger prawns | seasonal vegetables | jasmine rice</span>
-                        </div>
-                        <div className="mb-8">
-                            <span>lamb chops</span> 44.9<br />
-                            Lammkottlets (Neuseeland) |
-                            Wilder Brokkoli | getrüffeltes Kartoffelpüree<br />
-                            <span className="text-gray-500 text-sm">lamb chops (New Zealand) | wild broccoli | truffled mashed potatoes</span>
-                        </div>
-                        <div className="mb-8">
-                            <span>french duck red sin</span> 34.9<br />
-                            medium gegrillte Barbarieentenbrust |
-                            Saisongemüse | Rotwein-Hoisin Soße | Jasminreis<br />
-                            <span className="text-gray-500 text-sm">medium grilled barbary duck breast | seasonal vegetables | red wine hoisin sauce | jasmine rice</span>
-                        </div>
-                        <div className="mb-8">
-                            <span>salmon teriyaki</span> 31.9<br />
-                            Lachs | Saisongemüse |
-                            Teriyakisoße | Jasminreis<br />
-                            <span className="text-gray-500 text-sm">salmon | seasonal vegetables | teriyaki sauce | jasmine rice</span>
-                        </div>
-                    </div>
-                    {/* Right Column */}
-                    <div className="flex-1 min-w-[350px]">
-                        <div className="mb-8">
-                            <span>crispy angus tamarind</span> 38.9<br />
-                            panierte Black Angus Filetstreifen | grüner Spargel |
-                            Zuckerschoten | Zwiebeln | Tamarindensoße | Jasminreis<br />
-                            <span className="text-gray-500 text-sm">crispy black angus filet strips | green asparagus | sugar snap peas | onions | tamarind sauce | jasmine rice</span>
-                        </div>
-                        <div className="mb-8">
-                            <span>tiger prawns thai basil</span> 39.9<br />
-                            gegrillte Tiger Prawns | Saisongemüse | Thai-Basilikum | Jasminreis<br />
-                            <span className="text-gray-500 text-sm">grilled tiger prawns | seasonal vegetables | thai-basil | jasmine rice</span>
-                        </div>
-                        <div className="mb-8">
-                            <span>crispy tofu curry</span> 23.9<br />
-                            Tofu | Saisongemüse | rote Kokos-Curry Soße | Jasminreis<br />
-                            <span className="text-gray-500 text-sm">tofu | seasonal vegetables | red coconut-curry sauce | jasmine rice</span>
-                        </div>
+            <div className="bg-black text-white py-16 min-h-screen">
+                <div className="flex flex-col md:flex-row bg-black text-white p-4 md:p-10 items-center justify-center">
+                    <Image 
+                    src="/hauptspeiseSchrift.png" 
+                    alt="Main Dish" 
+                    width={170} 
+                    height={120}
+                    className="mb-6 md:mb-0 hidden md:block"
+                    />
+
+                    <div className="flex flex-col md:ml-10 w-full md:w-auto px-4 md:px-0">
+                        {menuItems.map((item, idx) => (
+                            <div key={idx} className="mb-6">
+                                <div className="flex flex-row items-center">
+                                    <span className="text-xl md:text-2xl font-medium">{item.title}</span>
+                                    <span className="text-base md:text-lg font-semibold ml-2">{item.price}</span>
+                                </div>
+                                <div className="flex flex-col">
+                                    <p className="text-gray-400 mt-1 text-sm md:text-base"> {language === "de" ? item.descriptionDE : item.descriptionEN}</p>
+                                </div>
+                            </div>
+                        ))}
                     </div>
                 </div>
             </div>
         </div>
-        </>
     );
 }
