8dc21b79ee35a5f5f82bb8fc8c400bf1a8c5aea4
thanhxt <thanhtran_xuan@hotmail.de>
2025-07-23 17:07:08 +0200

    changing landingPagevideo

--- src/components/landing-page.tsx
+++ src/components/landing-page.tsx
@@ -15,7 +15,7 @@ export default function LandingPage() {
                 playsInline
                 className="absolute inset-0 w-full h-full object-cover"
             >
-                <source src="/LandingPageVideo.mp4" type="video/mp4" />
+                <source src="/LandingPageVideo2.MOV" type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
 
